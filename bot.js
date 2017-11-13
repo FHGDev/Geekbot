5ca8ff3a4b0f7bbab31a294580f12a1ed3d31eba
FHGDev <gideon.lingle14@gmail.com>
2017-11-12 23:32:17 -0600

    Added a little something :3

--- bot.js
+++ bot.js
@@ -3,12 +3,15 @@ const bot = new discord.Client()
 
 bot.on("ready", () => {
   console.log("Geekbot v1.0 is online!")
-  bot.user.setGame("Geekbot v1.0 Loaded | Still testing")
+  bot.user.setGame("Geekbot v1.0 Loaded | Alpha Version...")
 })
 
 bot.on("message", message => {
   if (message.content == "Hey Geekbot!") {
-    message.reply("Hello, how are you doing?")
+    message.reply("hello, how are you doing?")
+  }
+  if (message.author.ID == '242734840829575169') {
+    message.author.sendMessage("GET TO YOUR WORK, HULK!")
   }
 })
 
