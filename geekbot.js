d3a3c5c73deabdd953261660b8c8ccc0c4178d8a
FHGDev <gideon.lingle14@gmail.com>
2017-11-12 22:58:16 -0600

    the startup file

---
+++ geekbot.js
@@ -0,0 +1,13 @@
+const discord = require('discord.js')
+const bot = new discord.Client()
+
+bot.on("ready" () => {
+  console.log("Geekbot v1.0 is online!")
+  bot.user.setGame("Geekbot v1.0 Loaded | Still testing")
+})
+
+bot.on("message", (message) => {
+  if (message.content == "Hey Geekbot!") {
+    message.reply("Hello, how are you doing?")
+  }
+})
