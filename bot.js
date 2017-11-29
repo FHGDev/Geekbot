2d174d6fb64ea6148b0dc0f47cc9fa02a1f0e53c
FHGDev <gideon.lingle14@gmail.com>
2017-11-29 17:09:04 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,6 @@
 const discord = require('discord.js')
 const bot = new discord.Client()
+const config = require('./config.json')
 
 bot.on("ready", () => {
   console.log("Geekbot v1.0 is online!")
@@ -10,7 +11,7 @@ bot.on("message", message => {
   if (message.content == "Hey Geekbot!") {
     message.reply("hello, how are you doing?")
   }
-  if (message.author.username == "FreakingHulk") {
+  if (message.author.id == config.hulk) {
     message.author.sendMessage("GET TO YOUR WORK, HULK!")
   }
 })
