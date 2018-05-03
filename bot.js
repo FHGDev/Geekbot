42f7e318e83a98d252a2205351e244d2f16c688a
FHGDev <gideon.lingle14@gmail.com>
2018-05-03 11:21:17 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,16 +4,19 @@ const config = require('./config.json')
 
 bot.on("ready", () => {
   console.log("Geekbot v1.0 is online!")
-  bot.user.setGame("Geekbot v1.0 Loaded | Alpha Version...")
+  bot.user.setActivity(`Loading Geekbot Alpha v1.0...`, {type: "STREAMING", url: "https://twitch.tv/freakinghulk"})
+  setTimeout(() => {
+    bot.user.setActivity(`for g!help | ${bot.guilds.size} server(s)`, {type: "WATCHING"})
+  }, 15000)
 })
 
 bot.on("message", message => {
   if (message.content == "Hey Geekbot!") {
-    message.reply("hello, how are you doing?")
+    message.channel.send(`Hey, <@${message.author.username}>, how are you doing?`)
   }
   if (message.author.id == config.hulk) {
     message.author.send("GET TO YOUR WORK, HULK!")
   }
 })
 
-bot.login(process.env.botToke);
+bot.login(process.env.botToken);
